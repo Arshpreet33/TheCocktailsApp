4b0a531bd066cdaed1450ab49443bff0beb7a4e4
Arshpreet33 <arshpreetsingh33@gmail.com>
2020-03-18 10:20:01 -0400

    Added fragments, created menu

---
+++ app/src/main/java/com/example/thecocktailsapp/Drink.java
@@ -0,0 +1,6 @@
+package com.example.thecocktailsapp;
+
+public class Drink {
+
+
+}
--- app/src/main/java/com/example/thecocktailsapp/MainActivity.java
+++ app/src/main/java/com/example/thecocktailsapp/MainActivity.java
@@ -1,14 +1,91 @@
 package com.example.thecocktailsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavController navController;
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupNavigation();
+    }
+
+    public void setupNavigation() {
+        toolbar = findViewById(R.id.toolbar_custom);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        menuItem.setCheckable(true);
+        drawerLayout.closeDrawers();
+
+        int menuID = menuItem.getItemId();
+
+        switch (menuID) {
+            case R.id.menu_item_home:
+                Toast.makeText(getApplicationContext(), "Home Item Clicked!", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.homeFragment);
+                break;
+            case R.id.menu_item_search:
+                Toast.makeText(getApplicationContext(), "Search Item Clicked!", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.searchFragment);
+                break;
+            case R.id.menu_item_disabled:
+                Toast.makeText(getApplicationContext(), "Disabled Item Clicked!", Toast.LENGTH_SHORT).show();
+                //navController.navigate(R.id.thirdFragment);
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), "Incorrect Menu Item Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout);
     }
 }
